f7b45c477675ddf5072438d4e462f37ef7f8e68d
Carl-Magnus <carl.magnus@hotmail.com>
2020-04-20 15:56:42 +0200

    First prototype of the player's movement controller as a script. Also added the player object as a prefab.

---
+++ Assets/Scripts/PlayerScripts/MovementController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    private float currentSpeed = 0f;
+    private float speedSmoothVelocity = 0f;
+    private float speedSmoothTime = 0.1f;
+    private float rotationSpeed = 0.1f;
+    private float gravity = 9f;
+
+    private Transform referenceTransform = null;
+
+    private CharacterController controller = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        referenceTransform = GameObject.FindGameObjectWithTag("MovementReference").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), (Input.GetAxisRaw("Vertical")));
+
+        Vector3 forward = referenceTransform.forward;
+        Vector3 right = referenceTransform.right;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
+        Vector3 gravityVector = Vector3.zero;
+
+        if (!controller.isGrounded)
+        {
+            gravityVector.y -= gravity;
+        }
+
+        if (desiredMoveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+        }
+
+        float targetSpeed = movementSpeed * movementInput.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        Mathf.Clamp(currentSpeed, 0, 6.5f);
+
+        controller.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
+        controller.Move(gravityVector * Time.deltaTime);
+    }
+}
