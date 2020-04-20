17a63dd782543dd912d134557cd2f0abcd7de447
skypekitten9 <victor.enhorning@gmail.com>
2020-04-20 16:35:47 +0200

    Added script to test input.

--- Assets/Prefabs/ControllerTestCube/ControllerTest.cs
+++ Assets/Prefabs/ControllerTestCube/ControllerTest.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ControllerTest : MonoBehaviour
 {
+    private InputController inputController;
     void Awake()
     {
+        inputController = new InputController();
+        inputController.Player.Jump.performed += ctx => TestInput();
+    }
 
+    private void OnEnable()
+    {
+        inputController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputController.Disable();
+    }
+
+    private void TestInput()
+    {
+        Debug.Log("Input test!");
     }
 }
+
