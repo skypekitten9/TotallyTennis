1c78e0e1b8656e01571d61a87b4413d7560f537b
skypekitten9 <victor.enhorning@gmail.com>
2020-06-03 13:39:52 +0200

    Added GameOver screen and canvas ingame, fixed a few packges prefabs.

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +10,8 @@ public class GameManager : MonoBehaviour
     int startScorePoints;
     public int lifePoints;
     int maxLifePoints;
+    GameObject gameCanvas, scoreText, heart1, heart2, heart3, gameOverPanel, gameOverCanvas, finalScoreText;
+    bool gameOver;
 
     void Start()
     {
@@ -15,6 +19,20 @@ public class GameManager : MonoBehaviour
         startScorePoints = scorePoints;
         lifePoints = 3;
         maxLifePoints = lifePoints;
+        gameOver = false;
+
+        gameCanvas = gameObject.transform.Find("GameCanvas").gameObject;
+        scoreText = gameCanvas.transform.Find("ScoreInt").gameObject;
+        heart1 = gameCanvas.transform.Find("Heart1").gameObject;
+        heart2 = gameCanvas.transform.Find("Heart2").gameObject;
+        heart3 = gameCanvas.transform.Find("Heart3").gameObject;
+
+        gameOverPanel = gameObject.transform.Find("GameOverPanel").gameObject;
+        gameOverCanvas = gameObject.transform.Find("GameOverCanvas").gameObject;
+        finalScoreText = gameOverCanvas.transform.Find("FinalScoreInt").gameObject;
+
+        gameOverPanel.SetActive(false);
+        gameOverCanvas.SetActive(false);
     }
 
     void Update()
@@ -24,11 +42,40 @@ public class GameManager : MonoBehaviour
 
     public void GainScore()
     {
-        scorePoints++;
+        if(!gameOver)
+        {
+            scorePoints += 10;
+            scoreText.GetComponent<Text>().text = scorePoints.ToString();
+        }
     }
 
     public void LoseLife()
     {
-        lifePoints--;
+        if(!gameOver)
+        {
+            if (lifePoints == 3)
+            {
+                heart3.SetActive(false);
+            }
+            else if (lifePoints == 2)
+            {
+                heart2.SetActive(false);
+            }
+            else
+            {
+                heart1.SetActive(false);
+                gameOver = true;
+                gameOverPanel.SetActive(true);
+                gameOverCanvas.gameObject.SetActive(true);
+                gameCanvas.gameObject.SetActive(false);
+                finalScoreText.gameObject.GetComponent<Text>().text = scorePoints.ToString();
+            }
+            lifePoints--;
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
